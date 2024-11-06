824710ba9406b1c49d4288f3282ce18e2688c592
zhoujing <121531845@qq.com>
2024-11-06 14:46:47 +0800

    1. add exp report generation

--- experiment_report_generation/document_loaders.py
+++ experiment_report_generation/document_loaders.py
@@ -1,7 +1,35 @@
 from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredWordDocumentLoader
 
-loader = Docx2txtLoader("./demo_exp_report.doc")
+def simple_loader():
+    loader = Docx2txtLoader("./demo_exp_report.docx")
 
-data = loader.load()
+    data = loader.load()
 
-print(data)
+    print(data)
+    # len of data is 1
+
+
+def unstructured_loader():
+
+
+    # loader = Docx2txtLoader("./demo_exp_report.docx")
+
+    # Retain Elements
+    loader = UnstructuredWordDocumentLoader("./demo_exp_report.docx", mode="elements")
+
+    datas = loader.load()
+    categorys = set()
+    for data in datas:
+        page_content = data.page_content
+        if '工作结束时间' in page_content:
+            pass
+        print('*' * 100)
+        print(data.metadata['category'])
+        categorys.add(data.metadata['category'])
+        print(data.page_content)
+    print(categorys)
+
+
+if __name__ == "__main__":
+    unstructured_loader()
